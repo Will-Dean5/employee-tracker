f47b9a8d7f5ac5490a875887c65947520e6e1330
Will D <will.dean0103@gmail.com>
2022-09-19 14:57:42 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const conTable = require('console.table');
+const path = require('path');
